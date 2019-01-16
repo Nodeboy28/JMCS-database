25f30806f3248303a0192b874749ac8665f27979
nodeboy <sudipta.just@gmail.com>
2019-01-16 23:42:11 +0600

    Start of Checkpoint

--- app.js
+++ app.js
@@ -3,6 +3,25 @@ var http = require('http');
 var mysql = require('mysql');
 var app = express();
 var bodyParser = require('body-parser');
+var multer = require('multer');
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname );
+  }
+});
+var fileFilter =function (req, file, cb) {
+ 
+  if(file.mimetype=== 'image/jpeg'){
+  	cb(null, true)
+  }
+  else{
+  	cb(null, false)
+  }
+};
+var upload = multer({ storage: storage, fileFilter: fileFilter });
 
 /**
 	parse all data
@@ -19,7 +38,6 @@ var now = new Date();
 app.set('view enjine', 'ejs');
 
 /**
-
 	used for date format
 **/
 app.use('/js', express.static(__dirname+ '/node_modules/bootstrap/dist/js'));
@@ -31,13 +49,12 @@ app.use('/fonts', express.static(__dirname+'./node_modules/font-awesome/fonts'))
 app.use('/static', express.static('./node_modules/font-awesome/css'));
 app.use('/public', express.static(__dirname + "/public"));
 app.use('/vendor', express.static(__dirname + "/vendor"));
-
-
+app.use('/uploads', express.static('uploads'));
 app.set('port', (process.env.PORT || 5000))
 const con = mysql.createConnection({
-  host     : 'http://138.201.49.53',
+  host     : 'localhost',
   user     : 'root',
-  password : '*utl488288#',
+  password : '',
   database : 'my_db'
 });
 con.connect(console.log('database connected'));
@@ -49,19 +66,51 @@ app.get('/',function(req,res){
 	});
 	res.render('./pages/index.ejs');
 });
+app.get('/department',function(req,res){
+	con.query("SELECT * FROM department", function (error, result) {
+	  		res.render('./pages/department.ejs',{
+	  			items : result
+	  		});
+	});
+});
+app.get('/department/delete/:id',function(req,res){
+	con.query('DELETE FROM department WHERE id = ?',req.params.id,function(error,results){
+		if(error) throw error;
+
+		console.log('deleted the item');
+
+	});
+	res.redirect('/department');
+});
+
+app.post('/department',function(req,res){
+	console.log(req.body);
+	var insertDeptData = {
+				id:req.body.id,
+				department_name:req.body.department_name,
+				
+			};
+			con.query("INSERT INTO department SET ? ", insertDeptData , function(err,result){
+				if(err)
+					throw err;
+				res.redirect('/department');
+			});
+});
 app.get('/form',function(req,res){
 	res.render('./pages/form.ejs');
 });
-app.post('/form',function(req,res){
-	console.log(req.body);
+app.post('/form',upload.single('Stdnt_image'),function(req,res){
+	 console.log(req.file);
 	var insertEventData = {
 				s_id:req.body.s_id,
+				form_details: req.body.form_details,
 				student_name:req.body.student_name,
 				blood_group:req.body.blood_group,
 				d_birth:req.body.d_birth,
 				religion:req.body.religion,
+				maritial_status:req.body.maritial_status,
 				tel_number:req.body.tel_number,
-				image: req.body.image,
+				Stdnt_image: req.file.path,
 				father_name:req.body.father_name,
 				mother_name:req.body.mother_name,
 				f_occupation:req.body.f_occupation,
@@ -93,7 +142,7 @@ app.post('/form',function(req,res){
 			con.query("INSERT INTO student_info SET ? ", insertEventData , function(err,result){
 				if(err)
 					throw err;
-				res.redirect(baseURL);
+				res.redirect('/table');
 			});
 });
 app.get('/table',function(req,res){
@@ -104,12 +153,25 @@ app.get('/table',function(req,res){
 	  		});
 	});
 });
-app.get('/details',function(req,res){
-	con.query('SELECT * FROM student_info WHERE s_id = ?',req.params.id, function (error, results) {
+app.get('/:id',function(req,res){
+	con.query('SELECT * FROM student_info WHERE s_id = ?',[req.params.id], function (error, results) {
 	  if (error) throw error;
-	  console.log('details: ', results);
+	  
+	  res.render('./pages/details.ejs',{
+	  	item:results
+	  });
 	});
-	res.render('./pages/details.ejs');
+	// res.render('./pages/details.ejs');
+	
+});
+app.get('/pages/delete/:id',function(req,res){
+	con.query('DELETE FROM student_info WHERE s_id = ?',req.params.id,function(error,results){
+		if(error) throw error;
+
+		console.log('deleted the item');
+
+	});
+	res.redirect('/table');
 });
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
